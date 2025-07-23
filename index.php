03d93da7e5a0c00e6d134e73356fc23338e3e041
NetworkPackets <anonym.set+networkpackets@gmail.com>
2025-07-23 14:04:43 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 $start_time = microtime(true);
 
 // ┌──────────── Config ─────────────┐
-$secret      = file_get_contents("secret");
 $logfile     = __DIR__ . '/' . $secret . '_packet_stats_all_interfaces.txt';
 $limit       = 20;
 $dateFormat  = 'Y-m-d H:i:s';
@@ -12,6 +11,9 @@ $textColor   = '#0f0';
 $headerBg    = '#050';
 $rowAltBg    = '#020';
 // └─────────────────────────────────┘
+
+//Read secret key from file secret
+$secret      = file_get_contents("secret");
 
 // Read log file once
 $lines = is_readable($logfile) ? file($logfile, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES) : [];
